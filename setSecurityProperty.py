eb884edcb80966fc6f3a94a34a82b8b6a6c6a96e
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2018-12-05 13:06:01 -0500

    Added sync with nodes

--- setSecurityProperty.py
+++ setSecurityProperty.py
@@ -1,3 +1,15 @@
 AdminTask.setAdminActiveSecuritySettings('[-customProperties["com.ibm.ws.security.web.logoutOnHTTPSessionExpire=true"]]')
 AdminConfig.save()
 print "Security property set"
+
+nodes = AdminConfig.list('Node').splitlines()
+for node in nodes:
+    nodeName = AdminConfig.showAttribute(node, 'name')
+    nodeagent = AdminControl.completeObjectName('type=NodeSync,process=nodeagent,node=' + nodeName + ',*')
+    if (len(nodeagent) > 0):
+        result = AdminControl.invoke(nodeagent, 'sync')
+
+        if (result == "true"):
+            print nodeName + " synchronized"
+        else:
+            print nodeName + " not synchronzied correctly"
