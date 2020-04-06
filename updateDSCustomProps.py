efaeec85709fe7d09f5fe6780ab2faf79717e468
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2020-04-06 09:14:44 -0400

    Update updateDSCustomProps.py

--- updateDSCustomProps.py
+++ updateDSCustomProps.py
@@ -1,15 +1,19 @@
-datasources = AdminConfig.list('DataSource').splitlines()
+providers = AdminConfig.getid('/JDBCProvider/DB2 Universal JDBC Driver Provider').splitlines()
+
+for provider in providers:
+
+	datasources = AdminConfig.list('DataSource', provider).splitlines()
   
-for datasource in datasources:
-        dsName = AdminConfig.showAttribute(datasource,'name')
-        print "Datasource: " + dsName
+	for datasource in datasources:
+	        dsName = AdminConfig.showAttribute(datasource,'name')
+	        print "Datasource: " + dsName
 
-        propertySet = AdminConfig.showAttribute(datasource,'propertySet')
+	        propertySet = AdminConfig.showAttribute(datasource,'propertySet')
 
-        sslName = ['name', 'sslConnection']
-        sslValue = ['value', 'true']
-        sslAttrs = [sslName, sslValue]
+	        sslName = ['name', 'sslConnection']
+	        sslValue = ['value', 'true']
+	        sslAttrs = [sslName, sslValue]
 
-        AdminConfig.create('J2EEResourceProperty', propertySet, sslAttrs)
+	        AdminConfig.create('J2EEResourceProperty', propertySet, sslAttrs)
 
 AdminConfig.save()
